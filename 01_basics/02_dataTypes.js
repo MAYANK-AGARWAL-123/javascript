cee28abe86d8f2059a8f91cd5f37625a5dc23065
Mayank <145209282+MAYANK-AGARWAL-123@users.noreply.github.com>
2024-07-01 13:37:11 +0000

    new files

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//alert("hello")
+
+console.log(33+3);
+console.log("Mayank");
+
+let name = "Mayank"
+let age = 18
+let isloggedIn = true;
+
+console.log(typeof age);
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "mayank1759.be21@chitkara.edu.in"
+var accountPassword = "1234"
+acccountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+accountEmail="something"
+accountPassword="5678"
+acccountCity="Benglore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,acccountCity,accountState])
